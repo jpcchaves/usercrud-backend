8b84f77953edd7775cb0f21c4ae83a6bc243735c
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-25 19:26:06 -0300

    feat: add spring validation to validate User's data from RequestBody

--- src/main/java/com/usercrud/usercrudbackend/controller/UserController.java
+++ src/main/java/com/usercrud/usercrudbackend/controller/UserController.java
@@ -3,10 +3,16 @@ package com.usercrud.usercrudbackend.controller;
 import com.usercrud.usercrudbackend.exception.UserNotFoundException;
 import com.usercrud.usercrudbackend.model.User;
 import com.usercrud.usercrudbackend.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("http://localhost:5173")
@@ -16,7 +22,7 @@ public class UserController {
   private UserRepository userRepository;
 
   @PostMapping("/user")
-  User newUser(@RequestBody User newUser) {
+  User newUser(@RequestBody @Valid User newUser) {
     return userRepository.save(newUser);
   }
 
@@ -32,7 +38,7 @@ public class UserController {
   }
 
   @PutMapping("/user/{id}")
-  User updateUser(@RequestBody User newUser, @PathVariable Long id){
+  User updateUser(@RequestBody @Valid User newUser, @PathVariable Long id){
     return userRepository.findById(id)
             .map(user -> {
               user.setUsername(newUser.getUsername());
@@ -50,6 +56,22 @@ public class UserController {
     }
     userRepository.deleteById(id);
     return "O usuário foi deletado com sucesso!";
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+
+      errors.put(fieldName, errorMessage);
+    });
+
+
+    return errors;
   }
 
 
--- src/main/java/com/usercrud/usercrudbackend/model/User.java
+++ src/main/java/com/usercrud/usercrudbackend/model/User.java
@@ -3,6 +3,10 @@ package com.usercrud.usercrudbackend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class User {
@@ -10,8 +14,19 @@ public class User {
   @Id
   @GeneratedValue
   private Long id;
+
+  @NotBlank(message = "O campo é obrigatório!")
+  @Pattern(regexp = "^[A-Z]+(.)*", message = "A primeira letra deve ser maiúscula!")
+  @Length(max = 12, message = "O nome de usuário pode ter no máximo 12 caracteres.")
   private String username;
+
+  @NotBlank(message = "O campo é obrigatório!")
+  @Pattern(regexp = "^[A-Z]+(.)*", message = "A primeira letra deve ser maiúscula!")
+  @Length(max = 12, message = "O nome pode ter no máximo 12 caracteres.")
   private String name;
+
+  @Email(message = "Insira um email válido!")
+  @NotBlank(message = "O campo é obrigatório!")
   private String email;
 
   public Long getId() {
